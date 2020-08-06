80b4d4b9cdcddc95f5f0654b99b430dba1f88b87
serhatyuna <serhat.yuna@ceng.deu.edu.tr>
2020-08-06 12:15:49 +0300

    migrations

--- SurveyApp.Web/Data/ApplicationDbContext.cs
+++ SurveyApp.Web/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SurveyApp.Web.Areas.Identity.Data;
 using SurveyApp.Web.Models;
 
 namespace SurveyApp.Web.Data
 {
-	public class ApplicationDbContext : IdentityDbContext
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 	{
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
 				: base(options)
---
+++ SurveyApp.Web/Data/Migrations/20200804150439_AddApplicationUser.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SurveyApp.Web.Data.Migrations
+{
+    public partial class AddApplicationUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+        }
+    }
+}
