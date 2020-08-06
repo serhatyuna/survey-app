472d25f98d22cba2cdf6a641d72b474047f2fd7c
serhatyuna <serhat.yuna@ceng.deu.edu.tr>
2020-08-06 12:17:27 +0300

    survey controller added

--- SurveyApp.Web/Controllers/HomeController.cs
+++ SurveyApp.Web/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -9,6 +10,7 @@ using SurveyApp.Web.Models;
 
 namespace SurveyApp.Web.Controllers
 {
+	// 	[Route("api/[controller]")]
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
@@ -23,9 +25,15 @@ namespace SurveyApp.Web.Controllers
 			return View();
 		}
 
-		public IActionResult Privacy()
+		[HttpGet("test/{surveyId}")]
+		public IActionResult Test(string surveyId)
 		{
-			return View();
+			return Content(surveyId);
+		}
+
+		public string Test2(string name, int numTimes = 1)
+		{
+			return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
 		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ SurveyApp.Web/Controllers/SurveyController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SurveyApp.Web.Areas.Identity.Data;
+using SurveyApp.Web.Models;
+using SurveyApp.Web.Services;
+
+namespace SurveyApp.Web.Controllers
+{
+	[Authorize]
+	public class SurveyController : Controller
+	{
+		private readonly SurveyService _surveyService;
+		private readonly UserManager<ApplicationUser> _userManager;
+
+		public SurveyController(SurveyService surveyService, UserManager<ApplicationUser> userManager)
+		{
+			_surveyService = surveyService;
+			_userManager = userManager;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var currentUser = await _userManager.GetUserAsync(User);
+			if (currentUser == null) return Challenge();
+			var surveys = await _surveyService.GetAllSurveys(currentUser);
+
+			var model = new SurveyListViewModel()
+			{
+				Surveys = surveys
+			};
+
+			return View(model);
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ActionName("Create")]
+		[ValidateAntiForgeryToken]
+		public IActionResult CreatePost(SurveyViewModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return RedirectToAction("Index");
+			}
+
+			return Ok();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
