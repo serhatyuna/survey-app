48b6d84d493ea1e1ccca25f0898c14961371a4e0
serhatyuna <serhat.yuna@ceng.deu.edu.tr>
2020-08-06 12:14:00 +0300

    custom user model created

---
+++ SurveyApp.Web/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SurveyApp.Web.Areas.Identity.Data
+{
+	public class ApplicationUser : IdentityUser
+	{
+		[PersonalData]
+		[Column(TypeName = "nvarchar(100)")]
+		public string FirstName { get; set; }
+
+		[PersonalData]
+		[Column(TypeName = "nvarchar(100)")]
+		public string LastName { get; set; }
+	}
+}
