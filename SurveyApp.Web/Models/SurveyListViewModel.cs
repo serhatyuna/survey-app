896e3bf50752fb69d0309ff01cdd72c75d0e3b92
serhatyuna <serhat.yuna@ceng.deu.edu.tr>
2020-08-06 12:16:06 +0300

    add view models for survey

---
+++ SurveyApp.Web/Models/SurveyListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyApp.Web.Models
+{
+	public class SurveyListViewModel
+	{
+		public Survey[] Surveys { get; set; }
+	}
+}
---
+++ SurveyApp.Web/Models/SurveyViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyApp.Web.Models
+{
+	public class SurveyViewModel
+	{
+		public int Id { get; set; }
+
+		[Display(Name = "Title")]
+		[Required]
+		public string Title { get; set; }
+
+		[Display(Name = "Created At")]
+		public DateTime CreatedAt { get; set; }
+
+		[Display(Name = "Updated At")]
+		public DateTime UpdatedAt { get; set; }
+
+		[Display(Name = "Questions")]
+		public List<Question> Questions { get; set; }
+
+		[Display(Name = "Answers")]
+		public List<FilledSurvey> FilledSurveys { get; set; }
+
+		[Display(Name = "# of Answers")]
+		public virtual int? CountOfFilledSurveys
+		{
+			get => FilledSurveys.Count();
+		}
+	}
+}
