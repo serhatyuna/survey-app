9c4fe776e12382ffa5108c8ad3c61baf83d81367
serhatyuna <serhat.yuna@ceng.deu.edu.tr>
2020-08-06 12:30:35 +0300

    survey service added

---
+++ SurveyApp.Web/Services/SurveyService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SurveyApp.Web.Areas.Identity.Data;
+using SurveyApp.Web.Data;
+using SurveyApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyApp.Web.Services
+{
+	public class SurveyService
+	{
+		private readonly ApplicationDbContext _context;
+
+		public SurveyService(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public Task<Survey[]> GetAllSurveys(ApplicationUser user)
+		{
+			return _context.Surveys.Where(s => s.UserId == user.Id).ToArrayAsync();
+		}
+	}
+}
--- SurveyApp.Web/Startup.cs
+++ SurveyApp.Web/Startup.cs
@@ -12,6 +12,7 @@ using SurveyApp.Web.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SurveyApp.Web.Services;
 
 namespace SurveyApp.Web
 {
@@ -28,13 +29,11 @@ namespace SurveyApp.Web
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddDbContext<ApplicationDbContext>(options =>
-					options.UseSqlServer(
-							Configuration.GetConnectionString("DefaultConnection")));
-			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-					.AddEntityFrameworkStores<ApplicationDbContext>();
+					options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 			services.AddRouting(options => options.LowercaseUrls = true);
 			services.AddControllersWithViews();
 			services.AddRazorPages();
+			services.AddScoped<SurveyService>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
