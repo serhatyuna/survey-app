0640aeb6a82b7cfc00b4ed62cc7af8f0ba487820
serhatyuna <serhat.yuna@ceng.deu.edu.tr>
2020-08-12 21:14:17 +0300

    crud methods of service implemented

--- SurveyApp.Web/Services/SurveyService.cs
+++ SurveyApp.Web/Services/SurveyService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 using SurveyApp.Web.Areas.Identity.Data;
 using SurveyApp.Web.Data;
 using SurveyApp.Web.Models;
@@ -18,9 +19,55 @@ namespace SurveyApp.Web.Services
 			_context = context;
 		}
 
-		public Task<Survey[]> GetAllSurveys(ApplicationUser user)
+		public async Task<Survey[]> GetAllSurveysAsync(ApplicationUser user)
 		{
-			return _context.Surveys.Where(s => s.UserId == user.Id).ToArrayAsync();
+			return await _context.Surveys
+				.Where(s => s.UserId == user.Id)
+				.Include(s => s.Questions)
+				.Include(s => s.FilledSurveys)
+				.ToArrayAsync();
+		}
+
+		public async Task<Survey> GetSurveyOfUserByIdAsync(int id, ApplicationUser user)
+		{
+			return await _context.Surveys
+				.Where(s => s.Id == id && s.UserId == user.Id)
+				.Include(s => s.Questions)
+					.ThenInclude(q => q.Options)
+				.Include(s => s.FilledSurveys)
+				.FirstOrDefaultAsync();
+		}
+
+		public async Task<Survey> GetSurveyByIdAsync(int id)
+		{
+			return await _context.Surveys
+				.Where(s => s.Id == id)
+				.Include(s => s.Questions)
+					.ThenInclude(q => q.Options)
+				.FirstOrDefaultAsync();
+		}
+
+		public async Task<bool> CreateSurveyAsync(SurveyViewModel model, ApplicationUser user)
+		{
+			var survey = new Survey()
+			{
+				Title = model.Title,
+				Questions = model.Questions,
+				CreatedAt = DateTime.Now,
+				UpdatedAt = DateTime.Now,
+				UserId = user.Id
+			};
+
+			_context.Surveys.Add(survey);
+			var saveResult = await _context.SaveChangesAsync();
+			return saveResult > 1;
+		}
+
+		public async Task<bool> DeleteSurveyAsync(Survey survey, ApplicationUser user)
+		{
+			_context.Surveys.Remove(survey);
+			var saveResult = await _context.SaveChangesAsync();
+			return saveResult > 1;
 		}
 	}
 }
