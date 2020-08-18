577c9291dc8f8c224be8d9a70fb2beb8c739f19a
Serhat Yuna <serhat.yuna@ceng.deu.edu.tr>
2020-08-18 17:09:31 +0300

    remove unused code

--- SurveyApp.Web/Controllers/HomeController.cs
+++ SurveyApp.Web/Controllers/HomeController.cs
@@ -10,7 +10,6 @@ using SurveyApp.Web.Models;
 
 namespace SurveyApp.Web.Controllers
 {
-	// 	[Route("api/[controller]")]
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
@@ -23,17 +22,6 @@ namespace SurveyApp.Web.Controllers
 		public IActionResult Index()
 		{
 			return View();
-		}
-
-		[HttpGet("test/{surveyId}")]
-		public IActionResult Test(string surveyId)
-		{
-			return Content(surveyId);
-		}
-
-		public string Test2(string name, int numTimes = 1)
-		{
-			return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
 		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
